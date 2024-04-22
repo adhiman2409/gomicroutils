16de133ac6396483fa83d4f8af8f232d7cd48fd4
adhiman2409 <dhiman.ashutosh@gmail.com>
2024-04-22 11:40:53 +0530

    Update organization.go

--- hrms/organization.go
+++ hrms/organization.go
@@ -7,18 +7,21 @@ import (
 )
 
 type Organization struct {
-	ID          primitive.ObjectID `bson:"_id"`
-	Domain      string             `bson:"domain"`
-	Name        string             `bson:"name"`
-	AdminId     string             `bson:"admin_id"`
-	EmailId     string             `bson:"email_id"`
-	PhoneNumber string             `bson:"phone_number"`
-	Status      string             `bson:"status"`
-	Logo        string             `bson:"logo"`
-	Address     string             `bson:"address"`
-	ZipCode     string             `bson:"zip_code"`
-	CountryCode string             `bson:"country_code"`
-	CreatedBy   string             `bson:"created_by"`
-	CreatedAt   time.Time          `bson:"created_at"`
-	UpdatedAt   time.Time          `bson:"updated_at"`
+	ID             primitive.ObjectID `bson:"_id"`
+	Domain         string             `bson:"domain"`
+	Name           string             `bson:"name"`
+	AdminId        string             `bson:"admin_id"`
+	EmailId        string             `bson:"email_id"`
+	PhoneNumber    string             `bson:"phone_number"`
+	Status         string             `bson:"status"`
+	Logo           string             `bson:"logo"`
+	Address        string             `bson:"address"`
+	ZipCode        string             `bson:"zip_code"`
+	CountryCode    string             `bson:"country_code"`
+	Description    string             `bson:"description"`
+	SocialAccounts []string           `bson:"social_accounts"`
+	Website        string             `bson:"website"`
+	CreatedBy      string             `bson:"created_by"`
+	CreatedAt      time.Time          `bson:"created_at"`
+	UpdatedAt      time.Time          `bson:"updated_at"`
 }
