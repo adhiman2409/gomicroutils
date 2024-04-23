87e04691548c6c20012c8744d11cf83abe1996e2
adhiman2409 <dhiman.ashutosh@gmail.com>
2024-04-23 07:42:38 +0530

    Update role.go

--- hrms/role.go
+++ hrms/role.go
@@ -11,6 +11,7 @@ const ORG_COLLECTION_ROLES = "roles"
 type Role struct {
 	ID          primitive.ObjectID `bson:"_id"`
 	DepId       string             `bson:"dep_id"`
+	DepName     string             `bson:"dep_name"`
 	Title       string             `bson:"title"`
 	IsActive    bool               `bson:"is_active"`
 	Description string             `bson:"description"`
