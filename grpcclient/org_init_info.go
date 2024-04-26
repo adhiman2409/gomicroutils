838f133e24a04f6310638b6c9fdb4fb062e93c53
adhiman2409 <dhiman.ashutosh@gmail.com>
2024-04-26 21:54:29 +0530

    Update org_init_info.go

--- grpcclient/org_init_info.go
+++ grpcclient/org_init_info.go
@@ -2,12 +2,14 @@ package grpcclient
 
 type OrgInitInfo struct {
 	Id                string
+	EID               string
 	Domain            string
 	OrgName           string
 	AdminEmailId      string
 	AdminPhoneNumber  string
 	AdminName         string
 	Department        string
+	Designation       string
 	EmailVerified     bool
 	Password          string
 	Role              string
