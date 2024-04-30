f3b01b3cb8fd76d327a3fe6dfcf4f3dbcfc38c33
vikrant <vikrant@pineswift.com>
2024-04-30 13:46:50 +0530

    code added

---
+++ grpcclient/org_attendance_requester.go
@@ -0,0 +1,35 @@
+package grpcclient
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/adhiman2409/gomicroutils/genproto/org"
+)
+
+func (a *OrgClient) GetAttendanceInfo(orgName string) (AttendanceInfo, error) {
+	req := org.AttendanceRequest{
+		OrgName: orgName,
+	}
+	res, err := a.client.GetAttendanceInfo(context.Background(), &req)
+	if err != nil {
+		fmt.Println(err.Error())
+		return AttendanceInfo{}, err
+	}
+	attendanceInfo := AttendanceInfo{
+		OrgId:                   res.OrgId,
+		OrgName:                 res.OrgName,
+		CaptureCheckInLocation:  res.CaptureCheckInLocation,
+		EnforceLocationCheckIn:  res.EnforceLocationCheckIn,
+		EnforceLocationCheckOut: res.EnforceLocationCheckOut,
+		RequiredLat:             res.RequiredLat,
+		RequiredLng:             res.RequiredLng,
+		FlexiHoursEnabled:       res.FlexiHoursEnabled,
+		FlexiHourWindowInMin:    res.FlexiHourWindowInMin,
+		OrgCheckInTime:          res.OrgCheckInTime,
+		OrgCheckOutTime:         res.OrgCheckOutTime,
+		CheckinMarginInMin:      res.CheckInMarginInMin,
+		OfficeHours:             res.OfficeHours,
+	}
+	return attendanceInfo, nil
+}
