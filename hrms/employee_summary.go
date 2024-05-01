03c311d279a2950819901af408093cc2d00becdb
adhiman2409 <dhiman.ashutosh@gmail.com>
2024-05-01 09:15:18 +0530

    Update employee_summary.go

--- hrms/employee_summary.go
+++ hrms/employee_summary.go
@@ -1,10 +1,32 @@
 package hrms
 
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
 type EmployeeSummary struct {
-	EmpId           string `bson:"emp_id"`
-	Name            string `bson:"name"`
-	Department      string `bson:"department"`
-	Designation     string `bson:"designation"`
-	Status          string `bson:"status"`
-	ProfileImageURL string `bson:"profile_image_url"`
+	ID                    primitive.ObjectID `bson:"_id"`
+	EmployeeId            string             `bson:"employee_id"`
+	FullName              string             `bson:"full_name"`
+	DOB                   string             `bson:"dob"`
+	PhoneNumber           string             `bson:"phone_number"`
+	EmailId               string             `bson:"email_id"`
+	EmployeeType          string             `bson:"employee_type"`
+	Department            string             `bson:"department"`
+	Designation           string             `bson:"designation"`
+	Role                  string             `bson:"role"`
+	EmploymentStatus      string             `bson:"employment_status"`
+	OfficeLocation        string             `bson:"office_location"`
+	WorkLocation          string             `bson:"work_location"`
+	ReportingManagerName  string             `bson:"reporting_manager"`
+	ReportingManagerId    string             `bson:"reporting_manager_id"`
+	ReportingManagerEmail string             `bson:"reporting_manager_email"`
+	Status                string             `bson:"status"`
+	ImgURL                string             `bson:"img_url"`
+	RegexText             string             `bson:"regex_text"`
+	CreatedBy             string             `bson:"created_by"`
+	CreatedAt             time.Time          `bson:"created_at"`
+	UpdatedAt             time.Time          `bson:"updated_at"`
 }
