09224152b4600c551f112f9f5d6d6ce702c968ce
vikrant <vikrant@pineswift.com>
2024-05-14 14:08:05 +0530

    update post.go

--- hrms/post.go
+++ hrms/post.go
@@ -4,6 +4,7 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type Post struct {
 	ID          primitive.ObjectID `bson:"_id"`
+	Topic       string             `bson:"topic"`
 	Content     string             `bson:"content"`
 	Image       string             `bson:"image"`
 	PostedBy    string             `bson:"posted_by"`
