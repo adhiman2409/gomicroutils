e9991c6c0bfaa8561181845855c5d9a02fdc5eed
vikrant <vikrant@pineswift.com>
2024-05-15 10:27:21 +0530

    code added

--- hrms/post.go
+++ hrms/post.go
@@ -1,13 +1,20 @@
 package hrms
 
-import "go.mongodb.org/mongo-driver/bson/primitive"
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
 
 type Post struct {
 	ID          primitive.ObjectID `bson:"_id"`
 	Topic       string             `bson:"topic"`
 	Content     string             `bson:"content"`
 	Image       string             `bson:"image"`
+	Important   bool               `bson:"important"`
 	PostedBy    string             `bson:"posted_by"`
 	Designation string             `bson:"designation"`
 	Date        string             `bson:"date"`
+	CreatedAt   time.Time          `bson:"created_at"`
+	UpdatedAt   time.Time          `bson:"updated_at"`
 }
