2882b06d577ae69b032fde1db33f0ad6e157d191
vikrant <vikrant@pineswift.com>
2024-07-04 22:00:03 +0530

    Update client.go

--- hrms/client.go
+++ hrms/client.go
@@ -4,5 +4,5 @@ type Client struct {
 	ClientID    string `bson:"client_id"`
 	Name        string `bson:"name"`
 	EmailID     string `bson:"email_id"`
-	PhoneNumber string `bso:"phone_number"`
+	PhoneNumber string `bson:"phone_number"`
 }
