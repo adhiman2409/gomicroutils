6113697c490b6e05b936b32a5834f4ac3780c7e6
vedant1523 <vedant@pineswift.com>
2024-07-15 11:47:56 +0530

    inventory file upload code modified

--- gcs/inventory_file_upload_response.go
+++ gcs/inventory_file_upload_response.go
@@ -1,12 +1,13 @@
 package gcs
 
 type InventoryFileUploadResponse struct {
-	BucketId    string   `json:"bucketId,omitempty"`
-	StoreId     string   `json:"storeId,omitempty"`
-	Domain      string   `json:"domain,omitempty"`
-	Department  string   `json:"department,omitempty"`
-	DocName     []string `json:"doc_name,omitempty"`
-	DocCategory []string `json:"doc_category,omitempty"`
-	DocType     string   `json:"doc_type,omitempty"`
-	DocPath     []string `json:"doc_path,omitempty"`
+	BucketId            string   `json:"bucketId,omitempty"`
+	StoreId             string   `json:"storeId,omitempty"`
+	Domain              string   `json:"domain,omitempty"`
+	Department          string   `json:"department,omitempty"`
+	DocName             []string `json:"doc_name,omitempty"`
+	DocCategory         []string `json:"doc_category,omitempty"`
+	DocType             string   `json:"doc_type,omitempty"`
+	ProductImagePathArr []string `json:"product_image_path_arr,omitempty"`
+	InvoicePathArr      []string `json:"invoice_path_arr,omitempty"`
 }
