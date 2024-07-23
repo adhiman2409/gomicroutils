9a69748477969a2532764e60681a33abb121e39a
vikrant <vikrant@pineswift.com>
2024-07-23 15:13:52 +0530

    fix

--- hrms/ticket.go
+++ hrms/ticket.go
@@ -12,28 +12,28 @@ type TicketConfig struct {
 	PrimaryEmployeeId      string             `bson:"primary_emp_id"`
 	PrimaryEmployeeName    string             `bson:"primary_emp_name"`
 	PrimaryEmployeeEmail   string             `bson:"primary_emp_email"`
-	SecondaryEmployeeId    string             `bson:"primary_emp_id"`
-	SecondaryEmployeeName  string             `bson:"primary_emp_name"`
-	SecondaryEmployeeEmail string             `bson:"primary_emp_email"`
+	SecondaryEmployeeId    string             `bson:"secondary_emp_id"`
+	SecondaryEmployeeName  string             `bson:"secondary_emp_name"`
+	SecondaryEmployeeEmail string             `bson:"secondary_emp_email"`
 	Queries                []string           `bson:"queries"`
 }
 
 type Ticket struct {
-	ID                     primitive.ObjectID `bson:"_id"`
-	TicketID               string             `bson:"ticket_id"`
-	Title                  string             `bson:"title"`
-	Description            string             `bson:"description"`
-	ReporterEmailId        string             `bson:"reporter_emailId"`
-	ReporterName           string             `bson:"reporter_name"`
-	ReporterDepartment     string             `bson:"reporter_department"`
-	AssigneeDepartment     string             `bson:"assignee_department"`
-	Status                 string             `bson:"status"`
-	PriorityLevel          string             `bson:"priority_level"`
-	TicketType             string             `bson:"ticket_type"`
-	Comments               []Comment          `bson:"comments"`
-	DueDate                string             `bson:"due_date"`
-	CreatedAt              time.Time          `bson:"created_at"`
-	UpdatedAt              time.Time          `bson:"updated_at"`
+	ID                 primitive.ObjectID `bson:"_id"`
+	TicketID           string             `bson:"ticket_id"`
+	Title              string             `bson:"title"`
+	Description        string             `bson:"description"`
+	ReporterEmailId    string             `bson:"reporter_emailId"`
+	ReporterName       string             `bson:"reporter_name"`
+	ReporterDepartment string             `bson:"reporter_department"`
+	AssigneeDepartment string             `bson:"assignee_department"`
+	Status             string             `bson:"status"`
+	PriorityLevel      string             `bson:"priority_level"`
+	TicketType         string             `bson:"ticket_type"`
+	Comments           []Comment          `bson:"comments"`
+	DueDate            string             `bson:"due_date"`
+	CreatedAt          time.Time          `bson:"created_at"`
+	UpdatedAt          time.Time          `bson:"updated_at"`
 }
 
 type Comment struct {
