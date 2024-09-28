07fd7738bfac00623832ad5d73e0adfefaf72d7c
adhiman2409 <dhiman.ashutosh@gmail.com>
2024-09-28 17:38:18 +0530

    Update download_file.go

--- gcs/download_file.go
+++ gcs/download_file.go
@@ -2,7 +2,6 @@ package gcs
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
@@ -54,18 +53,17 @@ func (a *StorageConnection) DownloadFile(w http.ResponseWriter, r *http.Request,
 }
 
 // Download gets a file from GCS bucket, Takes file path as a path param from request
-func (a *StorageConnection) DownloadImage(w http.ResponseWriter, r *http.Request) error {
+func (a *StorageConnection) DownloadImage(w http.ResponseWriter, r *http.Request, domain string) error {
 	pid := os.Getenv("GOOGLE_PROJECT_ID")
 	clientCtx, cancel := context.WithCancel(context.Background())
 	defer cancel()
-
-	var req ImageDownloadRequest
-	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
-		return err
-	}
-
-	nd := GetUpdatedDomain(req.Domain)
-	filePath := fmt.Sprintf("%s/%s/%s/%s/%s", req.Department, req.EID, req.Category, req.IType, req.Name)
+	department := mux.Vars(r)["department"]
+	eid := mux.Vars(r)["eid"]
+	category := mux.Vars(r)["category"]
+	documentType := mux.Vars(r)["dtype"]
+	filename := mux.Vars(r)["filename"]
+	nd := GetUpdatedDomain(domain)
+	filePath := fmt.Sprintf("%s/%s/%s/%s/%s", department, eid, category, documentType, filename)
 
 	reader, err := a.Client.Bucket(nd).UserProject(pid).Object(filePath).NewReader(clientCtx)
 	if err != nil {
@@ -82,7 +80,7 @@ func (a *StorageConnection) DownloadImage(w http.ResponseWriter, r *http.Request
 	}
 	w.Header().Set("Content-Type", contentType)
 	disposition := "inline"
-	w.Header().Set("Content-Disposition", disposition+"; filename="+req.Name)
+	w.Header().Set("Content-Disposition", disposition+"; filename="+filename)
 	w.Header().Set("Content-Length", size)
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
