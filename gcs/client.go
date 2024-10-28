3cca51642eed5f7288e4f096ae71e65ccd0437be
adhiman2409 <dhiman.ashutosh@gmail.com>
2024-10-28 22:45:09 +0530

    Update client.go

--- gcs/client.go
+++ gcs/client.go
@@ -16,7 +16,10 @@ type StorageConnection struct {
 // GetGCSClient gets singleton object for Google Storage
 func NewStorageConnection() *StorageConnection {
 	keyfilename := os.Getenv("KEY_FILE_NAME")
-	keyfilepath := "/app/cert/" + keyfilename
+	if keyfilename == "" {
+		keyfilename = "prodkeyfile.json"
+	}
+	keyfilepath := "./app/cert/" + keyfilename
 	storageClient, err := storage.NewClient(context.Background(), option.WithCredentialsFile(keyfilepath))
 	if err != nil {
 		log.Printf("failed to create gcs client error:%s", err.Error())
