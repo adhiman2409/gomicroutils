3d8e085373fce7f84c02722c20ad524ef53d2037
adhiman2409 <dhiman.ashutosh@gmail.com>
2024-11-11 06:51:55 +0530

    Update jobmgr_allocation_table.go

--- hrms/jobmgr_allocation_table.go
+++ hrms/jobmgr_allocation_table.go
@@ -4,7 +4,7 @@ import (
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
-type Recruiter struct {
+type RecruiterInfo struct {
 	Id   string `bson:"id"`
 	Name string `bson:"name"`
 }
@@ -12,5 +12,5 @@ type Recruiter struct {
 type AllocationEntry struct {
 	ID         primitive.ObjectID `bson:"_id"`
 	NextIndex  int64              `bson:"index"`
-	Recruiters []Recruiter        `bson:"recruiters"`
+	Recruiters []RecruiterInfo    `bson:"recruiters"`
 }
