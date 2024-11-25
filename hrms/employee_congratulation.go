f69e901227dc978a6235602531809fb92f41f823
adhiman2409 <dhiman.ashutosh@gmail.com>
2024-11-25 11:26:40 +0530

    Update employee_congratulation.go

--- hrms/employee_congratulation.go
+++ hrms/employee_congratulation.go
@@ -5,11 +5,20 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 type CongratulationList struct {
 	EmployeeId string `bson:"employee_id"`
 	Name       string `bson:"name"`
+	ImageURL   string `bson:"image_url"`
+	Remarks    string `bson:"remarks"`
+	CreatedAt  string `bson:"created_at"`
 }
 
 type CongratulationInfo struct {
 	ID                  primitive.ObjectID   `bson:"_id"`
+	Type                string               `bson:"type"`
+	Status              string               `bson:"status"`
+	Day                 int64                `bson:"day"`
+	Month               int64                `bson:"month"`
+	Year                int64                `bson:"year"`
 	EmployeeId          string               `bson:"employee_id"`
 	CongratulationCount int64                `bson:"congratulation_count"`
 	Congratulations     []CongratulationList `bson:"congratulations"`
+	CreatedAt           string               `bson:"created_at"`
 }
