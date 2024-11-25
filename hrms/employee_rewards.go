2e2f6e2c19df68cad7f022273978148750c007a8
adhiman2409 <dhiman.ashutosh@gmail.com>
2024-11-25 15:59:23 +0530

    Update employee_rewards.go

--- hrms/employee_rewards.go
+++ hrms/employee_rewards.go
@@ -2,7 +2,7 @@ package hrms
 
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
-type Congratulation struct {
+type Greeting struct {
 	EmployeeId string `bson:"employee_id"`
 	Name       string `bson:"name"`
 	ImageURL   string `bson:"image_url"`
@@ -11,25 +11,25 @@ type Congratulation struct {
 }
 
 type RewardInfo struct {
-	ID                  primitive.ObjectID `bson:"_id"`
-	EmployeeId          string             `bson:"employee_id"`
-	Type                string             `bson:"type"`
-	IsActive            bool               `bson:"is_active"`
-	Status              string             `bson:"status"`
-	AutoPublish         bool               `bson:"auto_publish"`
-	AutoUnpublish       bool               `bson:"auto_unpublish"`
-	PublishDay          int64              `bson:"publish_day"`
-	PublishMonth        int64              `bson:"publish_month"`
-	PublishYear         int64              `bson:"publish_year"`
-	UnpublishDay        int64              `bson:"unpublish_day"`
-	UnpublishMonth      int64              `bson:"unpublish_month"`
-	UnpublishYear       int64              `bson:"unpublish_year"`
-	BadgeURL            string             `bson:"badge_url"`
-	BadgeInfo           string             `bson:"badge_info"`
-	ImageURL            string             `bson:"image_url"`
-	Remarks             string             `bson:"remarks"`
-	CongratulationCount int64              `bson:"congratulation_count"`
-	Congratulations     []Congratulation   `bson:"congratulations"`
-	CreatedAt           string             `bson:"created_at"`
-	CreatedBy           string             `bson:"created_by"`
+	ID             primitive.ObjectID `bson:"_id"`
+	EmployeeId     string             `bson:"employee_id"`
+	Type           string             `bson:"type"`
+	IsActive       bool               `bson:"is_active"`
+	Status         string             `bson:"status"`
+	AutoPublish    bool               `bson:"auto_publish"`
+	AutoUnpublish  bool               `bson:"auto_unpublish"`
+	PublishDay     int64              `bson:"publish_day"`
+	PublishMonth   int64              `bson:"publish_month"`
+	PublishYear    int64              `bson:"publish_year"`
+	UnpublishDay   int64              `bson:"unpublish_day"`
+	UnpublishMonth int64              `bson:"unpublish_month"`
+	UnpublishYear  int64              `bson:"unpublish_year"`
+	BadgeURL       string             `bson:"badge_url"`
+	BadgeInfo      string             `bson:"badge_info"`
+	ImageURL       string             `bson:"image_url"`
+	Remarks        string             `bson:"remarks"`
+	GreetingsCount int64              `bson:"Greetings_count"`
+	Greetings      []Greeting         `bson:"Greetings"`
+	CreatedAt      string             `bson:"created_at"`
+	CreatedBy      string             `bson:"created_by"`
 }
