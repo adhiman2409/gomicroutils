13cd3a885222e1c43ed1b358980749d460049681
adhiman2409 <dhiman.ashutosh@gmail.com>
2024-12-08 22:27:08 +0530

    Update employee_attendance_regularization.go

--- hrms/employee_attendance_regularization.go
+++ hrms/employee_attendance_regularization.go
@@ -7,17 +7,26 @@ import (
 )
 
 type EmpAttendanceRegularization struct {
-	ID                  primitive.ObjectID `bson:"_id"`
-	EID                 string             `bson:"eid"`
-	Domain              string             `bson:"domain"`
-	StartDate           string             `bson:"start_date"`
-	EndDate             string             `bson:"end_date"`
-	IsLOPRegularization bool               `bson:"is_lop_regularization"`
-	CheckInTime         string             `bson:"check_in_time"`
-	CheckOutTime        string             `bson:"check_out_time"`
-	Reason              string             `bson:"reason"`
-	Status              string             `bson:"status"`
-	RegularizationCount int                `bson:"regularization_count"`
-	CreatedAt           time.Time          `bson:"created_at"`
-	UpdatedAt           time.Time          `bson:"updated_at"`
+	ID                primitive.ObjectID `bson:"_id"`
+	EmployeeId        string             `bson:"employee_id"`
+	EmployeeName      string             `bson:"employee_name"`
+	Domain            string             `bson:"domain"`
+	Day               int32              `bson:"day"`
+	Month             int32              `bson:"month"`
+	Year              int32              `bson:"year"`
+	CheckInTime       string             `bson:"check_in_time"`
+	CheckOutTime      string             `bson:"check_out_time"`
+	IsHalfDay         bool               `bson:"is_half_day"`
+	EmployeeRemarks   string             `bson:"employee_remarks"`
+	Status            string             `bson:"status"`
+	RequestedOn       string             `bson:"requested_on"`
+	RegularizedOn     string             `bson:"regularized_on"`
+	ManagerRemarks    string             `bson:"manager_remarks"`
+	IsHalfDayApproval bool               `bson:"is_half_day_approval"`
+	DocumentURL       string             `bson:"document_url"`
+	ManagerId         string             `bson:"manager_id"`
+	ManagerName       string             `bson:"manager_name"`
+	ManagerEmail      string             `bson:"manager_email"`
+	CreatedAt         time.Time          `bson:"created_at"`
+	UpdatedAt         time.Time          `bson:"updated_at"`
 }
