41d08a854f8b081890f1f7facb90392b5d610c54
adhiman2409 <dhiman.ashutosh@gmail.com>
2024-12-21 14:51:41 +0530

    Update employee_bank_info.go

--- hrms/employee_bank_info.go
+++ hrms/employee_bank_info.go
@@ -3,13 +3,15 @@ package hrms
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type BankInfo struct {
-	ID            primitive.ObjectID `bson:"_id"`
-	EmployeeId    string             `bson:"employee_id"`
-	FullName      string             `bson:"full_name"`
-	BankName      string             `bson:"bank_name"`
-	AccountNUmber string             `bson:"account_number"`
-	IFSC          string             `bson:"ifsc"`
-	ChequeURL     string             `bson:"cheque_url"`
-	CreatedAt     string             `bson:"created_at"`
-	UpdatedAt     string             `bson:"updated_at"`
+	ID              primitive.ObjectID `bson:"_id"`
+	EmployeeId      string             `bson:"employee_id"`
+	FullName        string             `bson:"full_name"`
+	BankName        string             `bson:"bank_name"`
+	AccountNUmber   string             `bson:"account_number"`
+	IsSalaryAccount bool               `bson:"is_salary_account"`
+	IFSC            string             `bson:"ifsc"`
+	ChequeURL       string             `bson:"cheque_url"`
+	CreatedAt       string             `bson:"created_at"`
+	UpdatedAt       string             `bson:"updated_at"`
+	UpdatedBy       string             `bson:"updated_by"`
 }
