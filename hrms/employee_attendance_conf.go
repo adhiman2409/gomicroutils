ea73101fb1ee4d4953656ca0cdeee911c8e2fc3b
adhiman2409 <dhiman.ashutosh@gmail.com>
2024-12-25 21:47:06 +0530

    Update employee_attendance_conf.go

--- hrms/employee_attendance_conf.go
+++ hrms/employee_attendance_conf.go
@@ -24,6 +24,7 @@ type EmpAttendanceConf struct {
 	WorkingDaysPerWeek      float32            `bson:"working_days_per_week"`
 	WeeklyOffDays           []string           `bson:"weekly_off_days"`
 	DailyWorkingHours       float32            `bson:"daily_working_hours"`
+	EmploymentStatus        string             `bson:"employment_status"`
 	CreatedAt               time.Time          `bson:"created_at"`
 	UpdatedAt               time.Time          `bson:"updated_at"`
 }
