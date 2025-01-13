863ca88dfee37c5966e893eb93097559b98b4007
adhiman2409 <dhiman.ashutosh@gmail.com>
2025-01-13 14:31:49 +0530

    Update employee_attendance_conf.go

--- hrms/employee_attendance_conf.go
+++ hrms/employee_attendance_conf.go
@@ -7,37 +7,38 @@ import (
 )
 
 type EmpAttendanceConf struct {
-	ID                       primitive.ObjectID `bson:"_id"`
-	EID                      string             `bson:"eid"`
-	Department               string             `bson:"department"`
-	ApplyOrgDefaultRules     bool               `bson:"apply_org_default_rules"`
-	CaptureCheckInLocation   bool               `bson:"capture_check_in_location"`
-	EnforceLocationCheckIn   bool               `bson:"enforce_location_check_in"`
-	EnforceLocationCheckOut  bool               `bson:"enforce_location_check_out"`
-	EnforceAttendanceSource  bool               `bson:"enforce_attendance_source"`
-	AttendanceSource         string             `bson:"attendance_source"`
-	RequiredLat              float64            `bson:"required_lat"`
-	RequiredLng              float64            `bson:"required_lng"`
-	FlexiHoursEnabled        bool               `bson:"flexi_hours_enabled"`
-	FlexiHourWindowInMin     int32              `bson:"flexi_hour_window_in_min"`
-	OrgCheckInTime           string             `bson:"org_check_in_time"`
-	OrgCheckOutTime          string             `bson:"org_check_out_time"`
-	CheckinMarginInMin       int32              `bson:"check_in_margin_in_min"`
-	WorkingDaysPerWeek       float32            `bson:"working_days_per_week"`
-	WeeklyOffDays            []string           `bson:"weekly_off_days"`
-	DailyWorkingHours        float32            `bson:"daily_working_hours"`
-	EmploymentStatus         string             `bson:"employment_status"`
-	IsWorkingInShifts        bool               `bson:"is_working_in_shifts"`
-	CurrentMonthShiftTimings []EmpShiftTimings  `bson:"current_month_shift_timings"`
-	NextMonthShiftTimings    []EmpShiftTimings  `bson:"next_month_shift_timings"`
-	CreatedAt                time.Time          `bson:"created_at"`
-	UpdatedAt                time.Time          `bson:"updated_at"`
+	ID                      primitive.ObjectID `bson:"_id"`
+	EID                     string             `bson:"eid"`
+	Department              string             `bson:"department"`
+	ApplyOrgDefaultRules    bool               `bson:"apply_org_default_rules"`
+	CaptureCheckInLocation  bool               `bson:"capture_check_in_location"`
+	EnforceLocationCheckIn  bool               `bson:"enforce_location_check_in"`
+	EnforceLocationCheckOut bool               `bson:"enforce_location_check_out"`
+	EnforceAttendanceSource bool               `bson:"enforce_attendance_source"`
+	AttendanceSource        string             `bson:"attendance_source"`
+	RequiredLat             float64            `bson:"required_lat"`
+	RequiredLng             float64            `bson:"required_lng"`
+	FlexiHoursEnabled       bool               `bson:"flexi_hours_enabled"`
+	FlexiHourWindowInMin    int32              `bson:"flexi_hour_window_in_min"`
+	OrgCheckInTime          string             `bson:"org_check_in_time"`
+	OrgCheckOutTime         string             `bson:"org_check_out_time"`
+	CheckinMarginInMin      int32              `bson:"check_in_margin_in_min"`
+	WorkingDaysPerWeek      float32            `bson:"working_days_per_week"`
+	WeeklyOffDays           []string           `bson:"weekly_off_days"`
+	DailyWorkingHours       float32            `bson:"daily_working_hours"`
+	EmploymentStatus        string             `bson:"employment_status"`
+	IsWorkingInShifts       bool               `bson:"is_working_in_shifts"`
+	CurrentShiftTimings     []EmpShiftTimings  `bson:"current_shift_timings"`
+	NextShiftTimings        []EmpShiftTimings  `bson:"next_shift_timings"`
+	CreatedAt               time.Time          `bson:"created_at"`
+	UpdatedAt               time.Time          `bson:"updated_at"`
 }
 
 type EmpShiftTimings struct {
 	Day               string  `bson:"day"`
 	Month             string  `bson:"month"`
 	Year              string  `bson:"year"`
+	Weekday           string  `bson:"weekday"`
 	IsOverlappedShift bool    `bson:"is_overlapped_shift"`
 	NumberOfShifts    int     `bson:"number_of_shifts"`
 	Shifts            []Shift `bson:"shifts"`
@@ -47,6 +48,7 @@ type EmpShiftTimings struct {
 }
 
 type Shift struct {
+	ShiftType       string `bson:"shift_type"`
 	OrgCheckInTime  string `bson:"check_in_time"`
 	OrgCheckOutTime string `bson:"check_out_time"`
 }
