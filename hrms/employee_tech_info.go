9131885a016b7844044e6ba745c26fbf925408df
adhiman2409 <dhiman.ashutosh@gmail.com>
2025-02-22 17:34:40 +0530

    Update employee_tech_info.go

--- hrms/employee_tech_info.go
+++ hrms/employee_tech_info.go
@@ -5,12 +5,11 @@ import (
 )
 
 type Experience struct {
-	StartYearAndMonth  string   `bson:"start_year_and_month"`
-	EndYearAndMonth    string   `bson:"end_year_and_month"`
-	ExperienceInMonths int      `bson:"experience_in_months"`
-	CompanyName        string   `bson:"company_name"`
-	Designation        string   `bson:"designation"`
-	Skills             []string `bson:"skills"`
+	StartYearAndMonth  string `bson:"start_year_and_month"`
+	EndYearAndMonth    string `bson:"end_year_and_month"`
+	ExperienceInMonths int    `bson:"experience_in_months"`
+	CompanyName        string `bson:"company_name"`
+	Designation        string `bson:"designation"`
 }
 
 type Project struct {
@@ -23,7 +22,7 @@ type Project struct {
 	Description      string   `bson:"description"`
 	CompanyName      string   `bson:"company_name"`
 	ClientName       string   `bson:"client_name"`
-	Skills           []string `bson:"skills"`
+	Skills           []string `bson:"s_skills"`
 	Domain           string   `bson:"domain"`
 	URL              string   `bson:"url"`
 }
@@ -53,12 +52,11 @@ type EmployeeTechInfo struct {
 	EmployeeId             string             `bson:"employee_id"`
 	About                  string             `bson:"about"`
 	TotalExperinceInMonths int                `bson:"total_experince_in_months"`
-	Skills                 []string           `bson:"skills"`
+	PSkill                 string             `bson:"p_skill"`
+	SSkills                []string           `bson:"s_skills"`
 	Experiences            []Experience       `bson:"experinces"`
 	Projects               []Project          `bson:"projects"`
 	Achivements            []Achivement       `bson:"achivements"`
 	Awards                 []Award            `bson:"awards"`
-	SkillsRegex            string             `bson:"skills_regex"`
-	DomainRegex            string             `bson:"domain_regex"`
 	IsProfileEditingLocked bool               `bson:"is_profile_editing_locked"`
 }
