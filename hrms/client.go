21d07238890a23d6eebc72607569e0a13b51802c
adhiman2409 <dhiman.ashutosh@gmail.com>
2025-02-26 19:15:11 +0530

    Update client.go

--- hrms/client.go
+++ hrms/client.go
@@ -1,8 +1,12 @@
 package hrms
 
 type Client struct {
-	ClientID    string `bson:"client_id"`
-	Name        string `bson:"name"`
-	EmailID     string `bson:"email_id"`
-	PhoneNumber string `bson:"phone_number"`
+	ClientName                  string `bson:"client_name"`
+	ProjectName                 string `bson:"project_name"`
+	IsInternalProject           bool   `bson:"is_internal_project"`
+	ReportingManagerName        string `bson:"reporting_manager_name"`
+	ReportingManagerEmailID     string `bson:"reporting_manager_email_id"`
+	ReportingManagerPhoneNumber string `bson:"reporting_manager_phone_number"`
+	IsClientBillable            bool   `bson:"is_client_billable"`
+	ReleaseDate                 string `bson:"release_date"`
 }
