4b9a013be1a019b9b20a84f78932678722f827d9
adhiman2409 <dhiman.ashutosh@gmail.com>
2025-03-07 13:04:48 +0530

    update

--- hrms/employee_tech_info.go
+++ hrms/employee_tech_info.go
@@ -59,6 +59,50 @@ type Feedback struct {
 	CreatorDesignation  string `bson:"creator_designation"`
 }
 
+// type SeparationDocument struct {
+// 	DocumentName string `bson:"document_name"`
+// 	DocumentURL  string `bson:"document_url"`
+// }
+
+// type SeparationInfo struct {
+// 	ResignationDate                   string               `bson:"resignation_date"`
+// 	EmployeeRemarks                   string               `bson:"resignation_remarks"`
+// 	ReportingManagerRemarks           string               `bson:"resignation_remarks"`
+// 	HumanResourceRemarks              string               `bson:"human_resource_remarks"`
+// 	ResignationAcceptanceDate         string               `bson:"resignation_acceptance_date"`
+// 	KnowledgeTransferStartDate        string               `bson:"knowledge_transfer_start_date"`
+// 	KnowledgeTransferEndDate          string               `bson:"knowledge_transfer_end_date"`
+// 	IsKnowledgeTransferCompleted      bool                 `bson:"is_knowledge_transfer_completed"`
+// 	KnowledgeTransferStatus           string               `bson:"knowledge_transfer_status"`
+// 	KnowledgeTransferOwnerId          string               `bson:"knowledge_transfer_owner_id"`
+// 	KnowledgeTransferOwnerName        string               `bson:"knowledge_transfer_owner_name"`
+// 	KnowledgeTransferOwnerDesignation string               `bson:"knowledge_transfer_owner_designation"`
+// 	KnowledgeTransferOwnerRemarks     string               `bson:"knowledge_transfer_owner_remarks"`
+// 	IsNoticePeriodServed              bool                 `bson:"is_notice_period_served"`
+// 	NoticePeriodStartDate             string               `bson:"notice_period_start_date"`
+// 	NoticePeriodEndDate               string               `bson:"notice_period_end_date"`
+// 	IsNoticePeriodExtended            bool                 `bson:"is_notice_period_extended"`
+// 	NoticePeriodExtensionDate         string               `bson:"notice_period_extension_date"`
+// 	IsNoticePeriodBuyoutRequested     bool                 `bson:"is_notice_period_buyout_requested"`
+// 	NoticePeriodBuyoutDays            string               `bson:"notice_period_buyout_days"`
+// 	NoticePeriodBuyoutStatus          string               `bson:"notice_period_buyout_staus"`
+// 	NoticePeriodBuyoutRemarks         string               `bson:"notice_period_buyout_remarks"`
+// 	NoticePeriodBuyoutAcceptanceDate  string               `bson:"notice_period_buyout_acceptance_date"`
+// 	NoticePeriodBuyoutRejectionDate   string               `bson:"notice_period_buyout_rejection_date"`
+// 	NoticePeriodBuyoutAcceptedBy      string               `bson:"notice_period_buyout_accepted_by"`
+// 	NoticePeriodBuyoutRejectedBy      string               `bson:"notice_period_buyout_rejected_by"`
+// 	IsExitInterviewScheduled          bool                 `bson:"is_exit_interview_scheduled"`
+// 	ExitInterviewDate                 string               `bson:"exit_interview_date"`
+// 	ExitInterviewerId                 string               `bson:"exit_interviewer_id"`
+// 	ExitInterviewerName               string               `bson:"exit_interviewer_name"`
+// 	ExitInterviewEmployeeRemarks      string               `bson:"exit_interview_employee_remarks"`
+// 	ExitInterviewerRemarks            string               `bson:"exit_interviewer_remarks"`
+
+// 	SeparationDocuments               []SeparationDocument `bson:"separation_documents"`
+
+// 	ExitDate                          string               `bson:"exit_date"`
+// }
+
 type EmployeeTechInfo struct {
 	ID                     primitive.ObjectID `bson:"_id"`
 	EmployeeId             string             `bson:"employee_id"`
