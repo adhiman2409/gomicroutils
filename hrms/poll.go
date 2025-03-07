a3c7985db02493c64f17585959d4331ffb9f4f87
adhiman2409 <dhiman.ashutosh@gmail.com>
2025-03-07 13:09:24 +0530

    Update poll.go

--- hrms/poll.go
+++ hrms/poll.go
@@ -1,6 +1,10 @@
 package hrms
 
-import "time"
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
 
 type PollOption struct {
 	Option    string `bson:"option"`
@@ -16,24 +20,24 @@ type Vote struct {
 }
 
 type Poll struct {
-	ID          string       `bson:"id"`
-	Title       string       `bson:"title"`
-	Content     string       `bson:"content"`
-	Options     []PollOption `bson:"options"`
-	Votes       []Vote       `bson:"vote"`
-	VoteCount   int          `bson:"vote_count"`
-	StartDate   string       `bson:"start_date"`
-	StartTime   string       `bson:"start_time"`
-	EndDate     string       `bson:"end_date"`
-	EndTime     string       `bson:"end_time"`
-	PostedBy    string       `bson:"posted_by"`
-	PostedById  string       `bson:"posted_by_id"`
-	IsPublished bool         `bson:"is_published"`
-	IsOpen      bool         `bson:"is_open"`
-	IsClosed    bool         `bson:"is_closed"`
-	ListDate    string       `bson:"list_date"`
-	UnlistDate  string       `bson:"unlist_date"`
-	CreatedAt   time.Time    `bson:"created_at"`
-	UpdatedAt   time.Time    `bson:"updated_at"`
-	Remarks     string       `bson:"remarks"`
+	ID          primitive.ObjectID `bson:"_id"`
+	Title       string             `bson:"title"`
+	Content     string             `bson:"content"`
+	Options     []PollOption       `bson:"options"`
+	Votes       []Vote             `bson:"vote"`
+	VoteCount   int                `bson:"vote_count"`
+	StartDate   string             `bson:"start_date"`
+	StartTime   string             `bson:"start_time"`
+	EndDate     string             `bson:"end_date"`
+	EndTime     string             `bson:"end_time"`
+	PostedBy    string             `bson:"posted_by"`
+	PostedById  string             `bson:"posted_by_id"`
+	IsPublished bool               `bson:"is_published"`
+	IsOpen      bool               `bson:"is_open"`
+	IsClosed    bool               `bson:"is_closed"`
+	ListDate    string             `bson:"list_date"`
+	UnlistDate  string             `bson:"unlist_date"`
+	CreatedAt   time.Time          `bson:"created_at"`
+	UpdatedAt   time.Time          `bson:"updated_at"`
+	Remarks     string             `bson:"remarks"`
 }
