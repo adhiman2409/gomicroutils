7513fd10843586824fa6cdfe42e6d2c55328d95e
adhiman2409 <dhiman.ashutosh@gmail.com>
2025-03-08 12:12:19 +0530

    Update poll.go

--- hrms/poll.go
+++ hrms/poll.go
@@ -13,31 +13,29 @@ type PollOption struct {
 }
 
 type Vote struct {
-	EmployeeId string    `bson:"employee_id"`
-	Option     string    `bson:"option"`
-	OptionId   string    `bson:"option_id"`
-	Date       time.Time `bson:"date"`
+	EmployeeId  string    `bson:"employee_id"`
+	Option      string    `bson:"option"`
+	OptionId    string    `bson:"option_id"`
+	DateAndTime time.Time `bson:"date_and_time"`
 }
 
 type Poll struct {
-	ID          primitive.ObjectID `bson:"_id"`
-	Title       string             `bson:"title"`
-	Content     string             `bson:"content"`
-	Options     []PollOption       `bson:"options"`
-	Votes       []Vote             `bson:"vote"`
-	VoteCount   int                `bson:"vote_count"`
-	StartDate   string             `bson:"start_date"`
-	StartTime   string             `bson:"start_time"`
-	EndDate     string             `bson:"end_date"`
-	EndTime     string             `bson:"end_time"`
-	PostedBy    string             `bson:"posted_by"`
-	PostedById  string             `bson:"posted_by_id"`
-	IsPublished bool               `bson:"is_published"`
-	IsOpen      bool               `bson:"is_open"`
-	IsClosed    bool               `bson:"is_closed"`
-	ListDate    string             `bson:"list_date"`
-	UnlistDate  string             `bson:"unlist_date"`
-	CreatedAt   time.Time          `bson:"created_at"`
-	UpdatedAt   time.Time          `bson:"updated_at"`
-	Remarks     string             `bson:"remarks"`
+	ID                primitive.ObjectID `bson:"_id"`
+	Title             string             `bson:"title"`
+	Content           string             `bson:"content"`
+	Options           []PollOption       `bson:"options"`
+	Votes             []Vote             `bson:"vote"`
+	VoteCount         int                `bson:"vote_count"`
+	StartDateAndTime  time.Time          `bson:"start_date_and_time"`
+	EndDateAndTime    time.Time          `bson:"end_date_and_time"`
+	PostedBy          string             `bson:"posted_by"`
+	PostedById        string             `bson:"posted_by_id"`
+	IsPublished       bool               `bson:"is_published"`
+	IsOpen            bool               `bson:"is_open"`
+	IsClosed          bool               `bson:"is_closed"`
+	ListDateAndTime   time.Time          `bson:"list_date_and_time"`
+	UnlistDateAndTime time.Time          `bson:"unlist_date_and_time"`
+	CreatedAt         time.Time          `bson:"created_at"`
+	UpdatedAt         time.Time          `bson:"updated_at"`
+	Remarks           string             `bson:"remarks"`
 }
