9ed7fa2a4d669375c2ef4dab057d7d8ea4450ca8
cb-adhiman <87748037+cb-adhiman@users.noreply.github.com>
2025-03-22 10:03:52 +0530

    Update notification.go

--- hrms/notification.go
+++ hrms/notification.go
@@ -4,7 +4,8 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type Device struct {
 	ID         primitive.ObjectID `bson:"_id,omitempty"`
-	DeviceId   string             `bson:"deviceId"`
-	DeviceType string             `bson:"deviceType"`
-	EmployeeId string             `bson:"employeeId"`
+	Token      string             `bson:"token"`
+	DeviceId   string             `bson:"device_id"`
+	DeviceType string             `bson:"device_type"`
+	EmployeeId string             `bson:"employee_id"`
 }
