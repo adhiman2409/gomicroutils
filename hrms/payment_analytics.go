0b181e2aaa3a646be41ded78a649273f472ef81b
cb-adhiman <151520737+adhiman2409@users.noreply.github.com>
2025-05-19 16:09:55 +0530

    Update payment_analytics.go

--- hrms/payment_analytics.go
+++ hrms/payment_analytics.go
@@ -1,17 +1,19 @@
 package hrms
 
+type HourlyUserData struct {
+	ActiveUserCount   int `bson:"active_user_count"`
+	InactiveUserCount int `bson:"inactive_user_count"`
+	TotalUserCount    int `bson:"total_user_count"`
+}
+
 type DailyUserData struct {
-	Day                    int   `bson:"day"`
-	HourlyActiveUserCount  []int `bson:"user_count"`
-	MaxActiveUserCount     int   `bson:"max_user_count"`
-	MinActiveUserCount     int   `bson:"min_user_count"`
-	TotalUserCount         int   `bson:"total_user_count"`
-	TotalInactiveUserCount int   `bson:"total_inactive_user_count"`
+	Day            int              `bson:"day"`
+	HourlyUserData []HourlyUserData `bson:"hourly_user_data"`
 }
 
 type MonthlyUserData struct {
 	TenantDomain   string          `bson:"tenant_domain"`
 	Month          int             `bson:"month"`
 	Year           int             `bson:"year"`
-	DailyUserCount []DailyUserData `bson:"daily_user_count"`
+	DailyUserCount []DailyUserData `bson:"daily_user_data"`
 }
