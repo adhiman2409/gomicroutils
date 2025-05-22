d2f3766ac519fc43a5d6a26eeae9ed0f371c7130
cb-adhiman <151520737+adhiman2409@users.noreply.github.com>
2025-05-22 13:55:07 +0530

    Update employee_timesheet.go

--- hrms/employee_timesheet.go
+++ hrms/employee_timesheet.go
@@ -6,18 +6,6 @@ import (
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
-type TimeSheetTask struct {
-	ID          string    `bson:"_id"`
-	TaskID      string    `bson:"task_id"`
-	Title       string    `bson:"title"`
-	Description string    `bson:"description"`
-	StartDate   time.Time `bson:"start_date"`
-	EndDate     time.Time `bson:"end_date"`
-	EmployeeID  string    `bson:"employee_id"`
-	ManagerID   string    `bson:"manager_id"`
-	CreatedBy   string    `bson:"created_by"`
-}
-
 type MonthlyTimeSheet struct {
 	ID             primitive.ObjectID `bson:"_id"`
 	Month          int                `bson:"month"`
@@ -29,6 +17,7 @@ type MonthlyTimeSheet struct {
 
 type TimeSheet struct {
 	Day               int              `bson:"day"`
+	Date              string           `bson:"date"`
 	FirstCheckInTime  time.Time        `bson:"first_check_in_time"`
 	LastCheckOutTime  time.Time        `bson:"last_check_out_time"`
 	TotalWorkingHours float64          `bson:"total_working_hours"`
@@ -40,7 +29,7 @@ type TimeSheet struct {
 }
 
 type TimeSheetEntry struct {
-	TaskID string  `bson:"task_id"`
-	Hours  float64 `bson:"hours"`
-	Remark string  `bson:"remark"`
+	ProjectId string  `bson:"project_id"`
+	Hours     float64 `bson:"hours"`
+	Remark    string  `bson:"remark"`
 }
