c19c197ece6f7ca1686daf2d7a20c23d715aec6b
cb-adhiman <151520737+adhiman2409@users.noreply.github.com>
2025-06-20 14:28:34 +0530

    Update zipkin.go

--- tracer/zipkin.go
+++ tracer/zipkin.go
@@ -6,18 +6,20 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"github.com/openzipkin/zipkin-go"
 	zipkinhttp "github.com/openzipkin/zipkin-go/middleware/http"
 )
 
 // ZipkinZapMiddleware logs trace/span info with zap after each request
-func ZipkinMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		t, _ := NewTracer("vms.unirms.com")
-		zipkinhttp.NewServerMiddleware(
-			t,
-			zipkinhttp.SpanName(mux.CurrentRoute(r).GetName()),
-			zipkinhttp.ServerTags(map[string]string{"component": "rest-api"}),
-		)
-		next.ServeHTTP(w, r)
-	})
+func ZipkinMiddleware(t *zipkin.Tracer) func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			zipkinhttp.NewServerMiddleware(
+				t,
+				zipkinhttp.SpanName(mux.CurrentRoute(r).GetName()),
+				zipkinhttp.ServerTags(map[string]string{"component": "rest-api"}),
+			)
+			next.ServeHTTP(w, r)
+		})
+	}
 }
