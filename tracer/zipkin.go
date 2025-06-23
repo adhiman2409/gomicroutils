f73f81ea1ad3ce57b38c853f9d36d4473866e075
cb-adhiman <151520737+adhiman2409@users.noreply.github.com>
2025-06-23 14:33:37 +0530

    Update zipkin.go

--- tracer/zipkin.go
+++ tracer/zipkin.go
@@ -5,32 +5,10 @@ import (
 
 	"net/http"
 
-	"github.com/gorilla/mux"
 	zipkinhttp "github.com/openzipkin/zipkin-go/middleware/http"
 )
 
 // ZipkinMiddleware injects Zipkin tracing into each HTTP request
 func ZipkinMiddleware() func(http.Handler) http.Handler {
-	return func(next http.Handler) http.Handler {
-		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			route := mux.CurrentRoute(r)
-			spanName := "unknown"
-			if route != nil {
-				if name := route.GetName(); name != "" {
-					spanName = name
-				}
-			}
-			host := r.Host
-			if host == "" {
-				host = "unknown"
-			}
-			spanName = host + "-" + spanName
-			middleware := zipkinhttp.NewServerMiddleware(
-				GetTracer(),
-				zipkinhttp.SpanName(spanName),
-				zipkinhttp.ServerTags(map[string]string{"component": "rest-api"}),
-			)
-			middleware(next).ServeHTTP(w, r)
-		})
-	}
+	return zipkinhttp.NewServerMiddleware(T)
 }
