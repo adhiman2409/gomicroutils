c1456891f398b89eefd634317af1120e78536f42
Ashutosh Dhiman <151520737+adhiman2409@users.noreply.github.com>
2025-07-19 12:20:36 +0530

    Update timex.go

--- timex/timex.go
+++ timex/timex.go
@@ -19,3 +19,23 @@ func GetLocalTime(timeZone string) (time.Time, error) {
 
 	return utcTime.In(loc), nil
 }
+
+func GetLocalDayMonthYear(timeZone string) (string, string, string, error) {
+	utcTime := time.Now().UTC()
+	backupTimeZone := "Asia/Kolkata" // Default time zone
+	if timeZone != "" {
+		backupTimeZone = timeZone
+	}
+	loc, err := time.LoadLocation(backupTimeZone)
+	if err != nil {
+		fmt.Println("Error loading location:", err)
+		return "", "", "", fmt.Errorf("invalid time zone: %s", backupTimeZone)
+	}
+
+	t := utcTime.In(loc)
+	day := fmt.Sprintf("%02d", t.Day())
+	month := fmt.Sprintf("%02d", t.Month())
+	year := fmt.Sprintf("%04d", t.Year())
+
+	return day, month, year, nil
+}
