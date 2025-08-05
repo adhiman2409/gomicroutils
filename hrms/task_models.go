f509c30a036c83e7f82e0d0645ea13fa3473f24d
Ashutosh Dhiman <151520737+adhiman2409@users.noreply.github.com>
2025-08-05 11:34:36 +0530

    Update task_models.go

--- hrms/task_models.go
+++ hrms/task_models.go
@@ -1,21 +1,17 @@
 package hrms
 
-import (
-	"time"
+import "time"
 
-	"go.mongodb.org/mongo-driver/bson/primitive"
-)
+type ProjectStateList struct {
+	ProjectId  string      `bson:"project_id"`
+	TaskStates []TaskState `bson:"task_states"`
+	CreatedAt  time.Time   `bson:"created_at"`
+}
 
 type TaskState struct {
-	ID          primitive.ObjectID `bson:"_id"`
-	ProjectId   string             `bson:"project_id"`
-	StateId     string             `bson:"state_id"`
-	Name        string             `bson:"name"`
-	Color       string             `bson:"color"`
-	Description string             `bson:"description"`
-	Icon        string             `bson:"icon"`
-	Order       string             `bson:"order"`
-	Default     bool               `bson:"default"`
-	UpdatedAt   time.Time          `bson:"updated_at"`
-	CreatedAt   time.Time          `bson:"created_at"`
+	StateId     string `bson:"state_id"`
+	Name        string `bson:"name"`
+	Color       string `bson:"color"`
+	Description string `bson:"description"`
+	Icon        string `bson:"icon"`
 }
