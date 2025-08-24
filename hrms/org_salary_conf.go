e6f39e1ac5d1d2cb7d2623626678ba8ea415b32e
Ashutosh Dhiman <151520737+adhiman2409@users.noreply.github.com>
2025-08-24 17:06:09 +0530

    Update org_salary_conf.go

--- hrms/org_salary_conf.go
+++ hrms/org_salary_conf.go
@@ -52,25 +52,36 @@ type OrgSalaryStructure struct {
 	TotalCTC              float32                   `bson:"total_ctc"`
 }
 
+type ESICConfig struct {
+	State                  string  `bson:"state"`
+	ESIEmployerPercentage  float32 `bson:"esi_employer_percentage"`
+	ESIEmployeePercentage  float32 `bson:"esi_employee_percentage"`
+	ESIMaximumWage         float32 `bson:"esi_maximum_wage"`
+	ESIMinimumWage         float32 `bson:"esi_minimum_wage"`
+	ESIApplicableWageLimit float32 `bson:"esi_applicable_wage_limit"`
+}
+
+type LWFConfig struct {
+	CityName                       string  `bson:"city_name"`
+	LWFEmployerMonthlyContribution float32 `bson:"lwf_employer_monthly_contribution"`
+	LWFEmployeeMonthlyContribution float32 `bson:"lwf_employee_monthly_contribution"`
+}
+
 type OrgSalaryConfig struct {
-	ID                                              primitive.ObjectID `bson:"_id"`
-	State                                           int                `bson:"state"`
-	Country                                         string             `bson:"country"`
-	DeliveryLocation                                string             `bson:"delivery_location"`
-	OrganizationDefaultTaxRegime                    string             `bson:"organization_default_tax_regime"` // e.g., "new", "old"
-	IsPFContributionIsBasedOnBasicSalary            bool               `bson:"is_pf_contribution_is_based_on_basic_salary"`
-	PFEmployerContributionPercentageOfBasic         float32            `bson:"pf_employer_contribution_percentage_of_basic"`
-	PFEmployeeContributionPercentageOfBasic         float32            `bson:"pf_employee_contribution_percentage_of_basic"`
-	PFMonthlyFixedEmployerContributionAmount        float32            `bson:"pf_monthly_fixed_employer_contribution_amount"`
-	PFMonthlyFixedEmployeeContributionAmount        float32            `bson:"pf_monthly_fixed_employee_contribution_amount"`
-	ESIMonthlyThresholdForEmployee                  float32            `bson:"esi_monthly_threshold_for_employee"`
-	ESIEmployerMonthlyContributionPercentageOfGross float32            `bson:"esi_employer_monthly_contribution_percentage_of_gross"`
-	ESIEmployeeMonthlyContributionPercentageOfGross float32            `bson:"esi_employee_monthly_contribution_percentage_of_gross"`
-	GratuityCalculationMethod                       string             `bson:"gratuity_calculation_method"` // e.g., "last_drawn_salary", "average_salary"
-	GratuityRatePerYear                             float32            `bson:"gratuity_rate_per_year"`      // e.g., 15 days' salary per year of service
-	GratuityMaxLimit                                float32            `bson:"gratuity_max_limit"`
-	LWFApplicableCityList                           []string           `bson:"lwf_applicable_city_list"`
-	LWFEmployerMonthlyContribution                  float32            `bson:"lwf_employer_monthly_contribution"`
-	LWFEmployeeMonthlyContribution                  float32            `bson:"lwf_employee_monthly_contribution"`
-	AnnualMedicalInsurancePremium                   float32            `bson:"annual_medical_insurance_premium"`
+	ID                                       primitive.ObjectID `bson:"_id"`
+	State                                    int                `bson:"state"`
+	Country                                  string             `bson:"country"`
+	DeliveryLocation                         string             `bson:"delivery_location"`
+	OrganizationDefaultTaxRegime             string             `bson:"organization_default_tax_regime"` // e.g., "new", "old"
+	IsPFContributionIsBasedOnBasicSalary     bool               `bson:"is_pf_contribution_is_based_on_basic_salary"`
+	PFEmployerContributionPercentageOfBasic  float32            `bson:"pf_employer_contribution_percentage_of_basic"`
+	PFEmployeeContributionPercentageOfBasic  float32            `bson:"pf_employee_contribution_percentage_of_basic"`
+	PFMonthlyFixedEmployerContributionAmount float32            `bson:"pf_monthly_fixed_employer_contribution_amount"`
+	PFMonthlyFixedEmployeeContributionAmount float32            `bson:"pf_monthly_fixed_employee_contribution_amount"`
+	GratuityCalculationMethod                string             `bson:"gratuity_calculation_method"` // e.g., "last_drawn_salary", "average_salary"
+	GratuityRatePerYear                      float32            `bson:"gratuity_rate_per_year"`      // e.g., 15 days' salary per year of service
+	GratuityMaxLimit                         float32            `bson:"gratuity_max_limit"`
+	ESICConfig                               []ESICConfig       `bson:"esi_config"`
+	LWFConfig                                []LWFConfig        `bson:"lwf_config"`
+	AnnualMedicalInsurancePremium            float32            `bson:"annual_medical_insurance_premium"`
 }
