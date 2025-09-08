d6742320905523b0bcf38a5a1851bc302a653931
Ashutosh Dhiman <151520737+adhiman2409@users.noreply.github.com>
2025-09-08 11:21:45 +0530

    Update collections.go

--- hrms/collections.go
+++ hrms/collections.go
@@ -163,15 +163,21 @@ const KYC_CONTACTS_COLLECTION = "kyc-contact_collection"
 
 const REJOVE_COLLECTION_DOCTORS = "rejove-doctors"
 const REJOVE_COLLECTION_DOCTOR_COUNTER = "rejove-doctor-id-counter"
-const REJOVE_COLLECTION_PATIENT_COUNTER = "rejove-patient-id-counter"
 const REJOVE_COLLECTION_DOCTOR_CASE_COUNTER = "rejove-doctor_case-id-counter"
 const REJOVE_COLLECTION_DOCTOR_PRACTICES = "rejove-doctor-practices"
 
+const REJOVE_COLLECTION_PATIENT_COUNTER = "rejove-patient-id-counter"
 const REJOVE_COLLECTION_PATIENTS = "rejove-patients"
+const REJOVE_COLLECTION_PATIENT_CASES = "rejove-patient_cases"
 
 func GetRejovePatientsCollection(client *mongo.Client, domain string) *mongo.Collection {
 	db := client.Database(strings.Replace(domain, ".", "_", -1))
 	return db.Collection(REJOVE_COLLECTION_PATIENTS)
+}
+
+func GetRejovePatientCasesCollection(client *mongo.Client, domain string) *mongo.Collection {
+	db := client.Database(strings.Replace(domain, ".", "_", -1))
+	return db.Collection(REJOVE_COLLECTION_PATIENT_CASES)
 }
 
 func GetRejoveDoctorsCollection(client *mongo.Client, domain string) *mongo.Collection {
