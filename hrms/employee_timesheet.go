6b1eb253d1a6cc6cead143743f370afd46c8594a
Ashutosh Dhiman <151520737+adhiman2409@users.noreply.github.com>
2025-09-09 14:37:10 +0530

    Update employee_timesheet.go

--- hrms/employee_timesheet.go
+++ hrms/employee_timesheet.go
@@ -25,6 +25,7 @@ type ProjectTask struct {
 	TaskID                   string    `bson:"task_id"`
 	TaskName                 string    `bson:"task_name"`
 	TaskWorkingHours         float64   `bson:"task_working_hours"`
+	AppliedOn                time.Time `bson:"applied_on"`
 	EmployeeRemarks          string    `bson:"employee_remarks"`
 	IsApproved               bool      `bson:"is_approved"`
 	ApprovedTaskWorkingHours float64   `bson:"approved_task_working_hours"`
