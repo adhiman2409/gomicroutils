c1cd9409361984e4a17896bef72011b44b11675c
Ashutosh Dhiman <151520737+adhiman2409@users.noreply.github.com>
2025-09-19 11:47:57 +0530

    Update employee_asset_counter.go

--- hrms/employee_asset_counter.go
+++ hrms/employee_asset_counter.go
@@ -3,7 +3,9 @@ package hrms
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type AssetIdCounter struct {
-	ID      primitive.ObjectID `bson:"_id"`
-	Prefix  string             `bson:"prefix"`
-	Counter int64              `bson:"counter"`
+	ID             primitive.ObjectID `bson:"_id"`
+	DepartmentID   string             `bson:"department_id"`
+	DepartmentName string             `bson:"department_name"`
+	Prefix         string             `bson:"prefix"`
+	Counter        int64              `bson:"counter"`
 }
