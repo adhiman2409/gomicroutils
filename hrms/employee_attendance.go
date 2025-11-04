8a95d8bd7b1882a44996fa72670fc3f32576a33e
Ashutosh Dhiman <151520737+adhiman2409@users.noreply.github.com>
2025-11-04 21:47:45 +0530

    Update employee_attendance.go

--- hrms/employee_attendance.go
+++ hrms/employee_attendance.go
@@ -7,12 +7,15 @@ import (
 )
 
 type GeoTracking struct {
-	Lat       float64   `bson:"lat"`
-	Lng       float64   `bson:"lng"`
-	Time      time.Time `bson:"time"`
-	EventName string    `bson:"event_name"` // e.g., "checkin", "checkout", "location_update"
-	EventId   string    `bson:"event_id"`   // Unique identifier for the event
-	Remarks   string    `bson:"remarks"`    // Additional information about the event
+	Lat                      float64   `bson:"lat"`
+	Lng                      float64   `bson:"lng"`
+	Time                     time.Time `bson:"time"`
+	Location                 string    `bson:"location"`
+	IsStationary             bool      `bson:"is_stationary"`
+	StationaryDurationInMins int64     `bson:"stationary_duration_in_mins"`
+	EventName                string    `bson:"event_name"` // e.g., "checkin", "checkout", "location_update"
+	EventId                  string    `bson:"event_id"`   // Unique identifier for the event
+	Remarks                  string    `bson:"remarks"`    // Additional information about the event
 }
 
 type EmployeeAttendance struct {
