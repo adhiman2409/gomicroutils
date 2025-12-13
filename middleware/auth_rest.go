a824bae873dd01e30c43735926ce7e0d44308131
Ashutosh Dhiman <151520737+adhiman2409@users.noreply.github.com>
2025-12-13 12:32:29 +0530

    Update auth_rest.go

--- middleware/auth_rest.go
+++ middleware/auth_rest.go
@@ -35,7 +35,7 @@ func RequestAuth(next http.Handler) http.Handler {
 		}
 
 		apiName := mux.CurrentRoute(r).GetName()
-		if apiName == "Login" || apiName == "Refresh" || apiName == "VerifyAuthEmail" || apiName == "ForgotPassword" || apiName == "OAuthGoogleCodeURL" || apiName == "UseGoogleAuth" || apiName == "UpdateEmployeeLocations" {
+		if apiName == "Login" || apiName == "Refresh" {
 			ai.Authorised = true
 			byteArray, _ = json.Marshal(ai)
 			ctx := context.WithValue(r.Context(), "claims", string(byteArray))
