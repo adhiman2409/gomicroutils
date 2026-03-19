b2ed29ae3e63cbc3f11590e7a494cba7a14da635
Ashutosh Dhiman <151520737+adhiman2409@users.noreply.github.com>
2026-03-19 20:40:32 +0530

    Update shift_type_enums.go

--- hrms/shift_type_enums.go
+++ hrms/shift_type_enums.go
@@ -2,45 +2,45 @@ package hrms
 
 type ShiftType int
 
-const (
-	Regular ShiftType = iota + 1
-	Morning
-	Evening
-	Night
-	WeeklyOff
-	Holiday
-	WorkFromHome
-)
+// const (
+// 	Regular ShiftType = iota + 1
+// 	Morning
+// 	Evening
+// 	Night
+// 	WeeklyOff
+// 	Holiday
+// 	WorkFromHome
+// )
 
-func (r ShiftType) String() string {
-	if r == 0 {
-		return "Regular"
-	}
-	return [...]string{"Regular", "Morning", "Evening", "Night", "WeeklyOff", "Holiday", "WorkFromHome"}[r-1]
-}
+// func (r ShiftType) String() string {
+// 	if r == 0 {
+// 		return "Regular"
+// 	}
+// 	return [...]string{"Regular", "Morning", "Evening", "Night", "WeeklyOff", "Holiday", "WorkFromHome"}[r-1]
+// }
 
-func (r ShiftType) EnumIndex() int {
-	return int(r)
-}
+// func (r ShiftType) EnumIndex() int {
+// 	return int(r)
+// }
 
-func GetAllShiftType() []string {
-	return []string{"Regular", "Morning", "Evening", "Night", "WeeklyOff", "Holiday", "WorkFromHome"}
-}
+// func GetAllShiftType() []string {
+// 	return []string{"Regular", "Morning", "Evening", "Night", "WeeklyOff", "Holiday", "WorkFromHome"}
+// }
 
-func ShiftTypeFromString(s string) ShiftType {
-	if s == "Morning" {
-		return Morning
-	} else if s == "Evening" {
-		return Evening
-	} else if s == "Night" {
-		return Night
-	} else if s == "WeeklyOff" {
-		return WeeklyOff
-	} else if s == "Holiday" {
-		return Holiday
-	} else if s == "WorkFromHome" {
-		return WorkFromHome
-	} else {
-		return Regular
-	}
-}
+// func ShiftTypeFromString(s string) ShiftType {
+// 	if s == "Morning" {
+// 		return Morning
+// 	} else if s == "Evening" {
+// 		return Evening
+// 	} else if s == "Night" {
+// 		return Night
+// 	} else if s == "WeeklyOff" {
+// 		return WeeklyOff
+// 	} else if s == "Holiday" {
+// 		return Holiday
+// 	} else if s == "WorkFromHome" {
+// 		return WorkFromHome
+// 	} else {
+// 		return Regular
+// 	}
+// }
