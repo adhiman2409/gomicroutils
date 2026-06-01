8d774c280f5250d638181424e77f35464f9b8f15
Ashutosh Dhiman <151520737+adhiman2409@users.noreply.github.com>
2026-06-01 21:51:38 +0530

    Update static_salary_staructure.go

--- hrms/static_salary_staructure.go
+++ hrms/static_salary_staructure.go
@@ -7,13 +7,15 @@ import (
 )
 
 type MonthlyPaidSalary struct {
-	Month              string  `bson:"month"`
-	Year               string  `bson:"year"`
-	GrossMonthlyAmount float32 `bson:"gross_monthly_amount"`
-	TaxAmount          float32 `bson:"tax_amount"`
-	AllowanceAmount    float32 `bson:"allowance_amount"`
-	NetMonthlyAmount   float32 `bson:"net_monthly_amount"`
-	Status             string  `bson:"status"` // Paid, Pending, Hold
+	Month                  string  `bson:"month"`
+	Year                   string  `bson:"year"`
+	GrossMonthlyAmount     float32 `bson:"gross_monthly_amount"`
+	TaxAmount              float32 `bson:"tax_amount"`
+	AllowanceAmount        float32 `bson:"allowance_amount"`
+	OtherTaxableEarnings   float32 `bson:"other_taxable_earnings"`
+	OtherTaxableDeductions float32 `bson:"other_taxable_deductions"`
+	NetMonthlyAmount       float32 `bson:"net_monthly_amount"`
+	Status                 string  `bson:"status"` // Paid, Pending, Hold
 }
 
 type CountrySalaryStructure struct {
