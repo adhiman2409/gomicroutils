2f976fb974c594cec2f63827b463df3e5106ff6a
Ashutosh Dhiman <151520737+adhiman2409@users.noreply.github.com>
2026-06-05 19:12:02 +0530

    Update marque.go

--- hrms/marque.go
+++ hrms/marque.go
@@ -7,13 +7,14 @@ import (
 )
 
 type Marque struct {
-	ID         primitive.ObjectID `bson:"_id"`
-	MarqueType string             `bson:"marque_type"`
-	Message    string             `bson:"message"`
-	DateFrom   string             `bson:"date_from"`
-	DateTo     string             `bson:"date_to"`
-	IsPrivate  bool               `bson:"is_private"`
-	IsRead     bool               `bson:"is_read"`
-	EmployeeID string             `bson:"employee_id,omitempty"`
-	CreatedAt  time.Time          `bson:"created_at,omitempty"`
+	ID                  primitive.ObjectID `bson:"_id"`
+	MarqueType          string             `bson:"marque_type"`
+	Message             string             `bson:"message"`
+	DateFrom            string             `bson:"date_from"`
+	DateTo              string             `bson:"date_to"`
+	IsPrivate           bool               `bson:"is_private"`
+	IsRead              bool               `bson:"is_read"`
+	EmployeeID          string             `bson:"employee_id,omitempty"`
+	GreetedByEmployeeID string             `bson:"greeted_by_employee_id,omitempty"`
+	CreatedAt           time.Time          `bson:"created_at,omitempty"`
 }
