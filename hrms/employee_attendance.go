bb8683a7ea007599135e5bb4800b532c1296527d
Ashutosh Dhiman <151520737+adhiman2409@users.noreply.github.com>
2026-07-13 14:53:03 +0530

    Update employee_attendance.go

--- hrms/employee_attendance.go
+++ hrms/employee_attendance.go
@@ -208,12 +208,12 @@ type DailyBreakStat struct {
 }
 
 type TenantAttendanceConfig struct {
-	ID                         primitive.ObjectID `bson:"_id"`
-	Domain                     string             `bson:"domain"`
-	RegularizationLimit        int                `bson:"regularization_limit"`
-	DefaultApproverRole        string             `bson:"default_approver_role"`
-	RegularizationManagerId    string             `bson:"regularization_manager_id"`
-	RegularizationManagerName  string             `bson:"regularization_manager_name"`
-	RegularizationManagerEmail string             `bson:"regularization_manager_email"`
-	UpdatedAt                  time.Time          `bson:"updated_at"`
+	ID                         primitive.ObjectID `bson:"_id" json:"id"`
+	Domain                     string             `bson:"domain" json:"domain"`
+	RegularizationLimit        int                `bson:"regularization_limit" json:"regularization_limit"`
+	DefaultApproverRole        string             `bson:"default_approver_role" json:"default_approver_role"`
+	RegularizationManagerId    string             `bson:"regularization_manager_id" json:"regularization_manager_id"`
+	RegularizationManagerName  string             `bson:"regularization_manager_name" json:"regularization_manager_name"`
+	RegularizationManagerEmail string             `bson:"regularization_manager_email" json:"regularization_manager_email"`
+	UpdatedAt                  time.Time          `bson:"updated_at" json:"updated_at"`
 }
