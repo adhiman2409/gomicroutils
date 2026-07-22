91d51a2b2c48d0f71c8c718685611d9d44a6135b
Ashutosh Dhiman <151520737+adhiman2409@users.noreply.github.com>
2026-07-22 13:50:21 +0530

    Update employee_reimbursement.go

--- hrms/employee_reimbursement.go
+++ hrms/employee_reimbursement.go
@@ -60,6 +60,7 @@ type Expense struct {
 	ReimbursementPaid       bool               `bson:"reimbursement_paid"`
 	ReimbursementPaidAmount float32            `bson:"reimbursement_paid_amount"`
 	ReimbursementPaidOn     time.Time          `bson:"reimbursement_paid_on"`
+	IsMarkedManuallyAsPaid  bool               `bson:"is_marked_manually_as_paid"`
 	Remarks                 []RemarksInfo      `bson:"remarks"`
 	DocURL                  string             `bson:"doc_url"`
 	Country                 string             `bson:"country"`
